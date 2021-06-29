115785caff7878a5f431191dfb64fddfd8a5d274
nhuse <nathan.huse@yahoo.com>
2021-06-29 11:40:41 -0500

    commit tree 65958e8c121629d6033d89ce11805fb660b87967

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, str) {
+    let filteredArr = arr.filter(function (driver) {return driver.toLowerCase() === str.toLowerCase()});
+    return filteredArr;
+}
+
+function fuzzyMatch(arr, str) {
+    let filteredDrivers = arr.filter(function (driver) {return driver.startsWith(str)});
+    return filteredDrivers;
+}
+
+function matchName(arr, str) {
+    let namedDriver = arr.filter(function (driver) {return (driver.name === str)});
+    return namedDriver;
+}
